2167dbe82fcfc2bf102ad52c011b56b2563fa427
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-18 11:17:51 +0200

    Create responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='mt-8 p-4 bg-primary'>
-        <div className='mx-8 text-center'>
-            <div>
-                <a href="#" className='uppercase text-lg'>Paulith</a>
-            </div>
+    <footer className='w-full text-center border-t border-gray-200 py-4'>
+      <div className='flex flex-col sm:flex-row justify-between items-center mx-[1.5rem] md:mx-[2.5rem] text-yellow-600'>
+        <div className='text-sm'>
+          &copy; 2024 PAULITH STORE
         </div>
+        <div className='flex space-x-6 sm:mt-0 mt-2 text-sm'>
+          <a href="#" className='hover:text-yellow-700'>
+            O NAS
+          </a>
+          <a href="#" className='hover:text-yellow-700'>
+            REGULAMIN
+          </a>
+          <a href="#" className='hover:text-yellow-700'>
+            POLITYKA PRYWATNOŚCI
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
