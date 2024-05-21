f064ff975fd237a1d5afad1cbd27bc3f3ae044c6
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-21 10:46:32 +0200

    Create text banner for ProjectSection

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,25 +1,12 @@
 import React from 'react'
-import ProjectImg from "../../assets/ProjectSection/ProjectImg.jpg";
 
 const ProjectSection = () => {
   return (
-    <div className="relative">
-      <div className="relative">
-        <img
-          src={ProjectImg}
-          alt="#"
-          className="w-full aspect-[4/3] md:aspect-[5/3] lg:aspect-[7/2]
-          object-cover"
-        />
-        <div
-          className="absolute inset-0 flex items-center justify-center
-          bg-black/25"
-        >
-          <span className="text-white font-lato text-xl md:text-3xl uppercase text-center">
-            Jeżeli masz gotowy projekt lub pomysł na wykonanie biżuterii zapraszam do kontaktu na adres e-mail
-          </span>
-        </div>
-      </div>
+    <div className='text-yellow-600 text-center text-[1.1rem]'>
+      <p>Przenieś swoje marzenia do rzeczywistości dzięki naszej wyjątkowej ofercie biżuterii na zamówienie!</p>
+      <p>Stwórz unikalne dzieło, które będzie odzwierciedlać Twój styl i osobowość.</p>
+      <p>Zrealizujemy Twoją wizję z najwyższą starannością i precyzją.</p>
+      <p>Skontaktuj się ze mną i ciesz się biżuterią, która jest naprawdę Twoja!</p>
     </div>
   );
 };
