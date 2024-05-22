041c0c280b9f8329262c4518141ddf7517085717
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-22 10:34:44 +0200

    Create contact section and add styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,68 +1,42 @@
 import React from 'react';
+import FormField from '../Shared/FormField';
+import TextArea from '../Shared/TextArea';
 
 const Contact = () => {
   return (
-    <div className='container pb-12 border-b-2'>
-        <form action="#" method='POST'>
-            <div className='grid grid-cols-2 grid-rows-2 gap-4'>
-                <div>
-                    <label for='firstName' className='block'>Imię</label>
-                    <input
-                        type="text"
-                        name='firstName'
-                        placeholder='Imię...'
-                        className='mt-1 p-2 w-full border rounded-md'
-                    />
-                </div>
-                <div>
-                    <label for='lastName' className='block'>Nazwisko</label>
-                    <input
-                        type="text"
-                        name='lastName'
-                        placeholder='Nazwisko...'
-                        className='mt-1 p-2 w-full border rounded-md'
-                    />
-                </div>
-                <div>
-                    <label for='email' className='block'>E-mail</label>
-                    <input
-                        type="email"
-                        name='email'
-                        placeholder='Adres e-mail...'
-                        className='mt-1 p-2 w-full border rounded-md'
-                    />
-                </div>
-                <div>
-                    <label for='topic' className='block'>Temat</label>
-                    <input
-                        type="text"
-                        name='topic'
-                        placeholder='Temat...'
-                        className='mt-1 p-2 w-full border rounded-md'
-                    />
-                </div>
-            </div>
-
-            <div className='mt-4'>
-                <label for='message' className='block'>Wiadomość</label>
-                <textarea
-                    name="message" 
-                    rows="5"
-                    placeholder='Treść wiadomości...'
-                    className='resize-none w-full mt-1 p-2 border rounded-md'
-                ></textarea>
-            </div>
-
-            <div className='mt-6 text-center'>
-                <button
-                    type='submit'
-                    className='p-3 bg-blue-500 text-white rounded-md
-                    hover:bg-blue-600 duration-500'
-                >
-                    Wyślij wiadomość
-                </button>
-            </div>
-        </form>
+    <div className='mb-10 flex items-center justify-center'>
+      <form>
+        <div className='md:flex md:gap-5'>
+          <FormField
+            type='text'
+            id='name'
+            label='Imię'
+            placeholder='Imię...'
+          />
+          <FormField
+            type='email'
+            id='email'
+            label='E-mail'
+            placeholder='Email...'
+          />
+        </div>
+        <FormField
+          type='text'
+          id='subject'
+          label='Temat'
+          placeholder='Temat...'
+        />
+        <TextArea />
+        <div className='flex items-center justify-center'>
+          <button
+            type='submit'
+            className='bg-yellow-800 hover:bg-yellow-900 text-white
+            font-semibold py-2 px-6 rounded'
+          >
+            Wyślij Wiadomość
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
