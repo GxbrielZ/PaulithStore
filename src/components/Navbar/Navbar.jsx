8106008c81d66b80d65522d93994ff81ab618bf2
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-24 09:23:03 +0200

    Update code to use CartButton component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import PaulithLogo from "../../assets/Logo/PaulithLogo.png";
 import MenuLinks from '../../data/MenuLinks';
 import NavbarLinks from './NavbarLinks';
+import CartButton from './CartButton';
 import ToggleMenu from './ToggleMenu';
-import { MdOutlineShoppingBag } from "react-icons/md";
-import { IoMenu, IoClose } from "react-icons/io5";
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -24,37 +23,8 @@ const Navbar = () => {
             >
                 Paulith
             </a>
-
-            {/* Navbar Center */}
             <NavbarLinks />
-
-            {/* Navbar Right Side */}
-            <div className='flex items-center'>
-                <button className='relative p-3'>
-                    <MdOutlineShoppingBag className='text-2xl text-white' />
-                        <div
-                            className='w-4 h-4 bg-red-500
-                            text-white rounded-full absolute top-0 right-0
-                            flex items-center justify-center text-xs'
-                        >
-                            4
-                        </div>
-                </button>
-                {nav ? (
-                    <IoClose
-                        onClick={handleNav}
-                        className='text-2xl text-white cursor-pointer
-                        z-10 md:hidden'
-                    />
-                ) : (
-                    <IoMenu
-                        onClick={handleNav}
-                        className='text-2xl text-white cursor-pointer md:hidden'
-                    />
-                )}
-            </div>
-
-            {/* Toggle Menu */}
+            <CartButton nav={nav} handleNav={handleNav} />
             <ToggleMenu nav={nav} handleNav={handleNav} />
         </div>
     );
