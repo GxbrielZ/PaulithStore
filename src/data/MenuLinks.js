1e844a7023faf4182a57f343393e9f15727ee6e8
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-24 08:41:01 +0200

    Add file that contains array with links

---
+++ src/data/MenuLinks.js
@@ -0,0 +1,24 @@
+const MenuLinks = [
+    {
+        id: 1,
+        name: "Strona Główna",
+        link: "/#home"
+    },
+    {
+        id: 2,
+        name: "Sklep",
+        link: "/#shop"
+    },
+    {
+        id: 3,
+        name: "Regulamin",
+        link: "/#rules"
+    },
+    {
+        id: 4,
+        name: "Kontakt",
+        link: "/#contact"
+    },
+];
+
+export default MenuLinks;
