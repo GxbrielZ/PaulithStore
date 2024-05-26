2f0247e2d8469678c403a1cfcfbc9baf80ddb3c6
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-26 18:24:08 +0200

    Create MobileMenu component to show menu on mobile devices

---
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MenuLinks from '../../data/MenuLinks';
+import { Link } from 'react-router-dom';
+
+const MobileMenu = () => {
+  return (
+    <div
+        className='fixed top-0 left-0 w-full min-h-screen bg-white
+        z-40 flex flex-col justify-start md:hidden
+        py-[6.5rem] pl-[1.5rem]'
+    >
+        <ul className='flex flex-col gap-10 text-2xl'>
+            {MenuLinks.map(link => (
+                <li key={link.id}>
+                    <Link
+                        to={link.link}
+                        className='text-yellow-800 uppercase hover:text-yellow-600'
+                    >
+                        {link.name}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </div>
+  );
+};
+
+export default MobileMenu;
