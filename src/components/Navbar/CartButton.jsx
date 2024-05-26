03a228240b7391378929056173d31a6fc4a47a62
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-26 14:57:16 +0200

    Remove toggle menu code

--- src/components/Navbar/CartButton.jsx
+++ src/components/Navbar/CartButton.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { MdOutlineShoppingBag } from "react-icons/md";
-import { IoMenu, IoClose } from "react-icons/io5";
 
-const CartButton = ({ nav, handleNav }) => {
+const CartButton = () => {
   return (
     <div className='flex items-center'>
         <button className='relative p-3 text-2xl text-yellow-800 hover:text-yellow-600'>
@@ -15,18 +14,6 @@ const CartButton = ({ nav, handleNav }) => {
                     4
                 </div>
         </button>
-        {nav ? (
-            <IoClose
-                onClick={handleNav}
-                className='text-2xl text-yellow-800 cursor-pointer
-                z-10 md:hidden'
-            />
-        ) : (
-            <IoMenu
-                onClick={handleNav}
-                className='text-2xl text-yellow-800 cursor-pointer md:hidden'
-            />
-        )}
     </div>
   );
 };
