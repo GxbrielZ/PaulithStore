699d5c2d9c52311cde692fb49704dbb8a3df40ff
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-05-31 10:51:44 +0200

    Update hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Bracelet from "../../assets/Categories/Bracelet.jpg";
+import HeroImg1 from "../../assets/Hero/HeroImg1.jpg"
+import HeroImg2 from "../../assets/Hero/HeroImg2.jpg"
 
 const Hero = () => {
     return (
       <div className="px-4 md:px-20 text-yellow-800 font-merriweather">
-        <h1 className="text-6xl text-center italic py-8">
-            Magia Biżuterii
+        <h1 className='text-center text-4xl italic my-6'>
+          Magia Biżuterii
         </h1>
         <div
           className="grid grid-cols-1 gap-1
           md:[grid-template-columns:1fr_2fr]"
         >
           <img
-            src={Bracelet}
+            src={HeroImg1}
             alt="Jewelry 1"
-            className="w-full aspect-[4/5] object-cover"
+            className="w-full aspect-[4/5] object-cover rounded-l-xl brightness-90"
           />
-          <div className="">
+          <div>
             <img
-                src={Bracelet}
+                src={HeroImg2}
                 alt="Jewelry 3"
-                className="w-full aspect-[15/5] object-cover"
+                className="w-full aspect-[16/5] object-cover rounded-tr-xl brightness-90"
             />
-            <p className='mt-4 mx-6 text-3xl italic'>
-              Odkryj świat, w którym każdy klejnot opowiada swoją historię. <br/>Nasza ręcznie robiona biżuteria z minerałów łączy naturalne piękno i precyzję, tworząc niezwykłe dzieła sztuki. <br/>Przywitaj nową erę elegancji z biżuterią, która każdego dnia wprowadza magię i piękno do Twojej codzienności.
-            </p>
-            <div className='flex justify-end mt-6'>
-              <Link
-                to="/#"
-                className="bg-yellow-800 hover:bg-yellow-900 text-white font-semibold py-2 px-12 rounded"
-              >
-                Odkryj
-              </Link>
+            <div
+              className='flex items-center bg-yellow-500 bg-opacity-20
+              aspect-[16/5] px-10 rounded-br-xl'
+            >
+              <p className='text-3xl italic'>
+                Odkryj świat, w którym każdy klejnot opowiada swoją historię. <br/>Nasza ręcznie robiona biżuteria z minerałów łączy naturalne piękno i precyzję, tworząc niezwykłe dzieła sztuki. <br/>Przywitaj nową erę elegancji z biżuterią, która każdego dnia wprowadza magię i piękno do Twojej codzienności.
+              </p>
+              <div className=''>
+                <Link
+                  to="/#"
+                  className="bg-yellow-800 hover:bg-yellow-900 text-white font-semibold py-2 px-12 rounded"
+                >
+                  Odkryj
+                </Link>
+              </div>
             </div>
           </div>
         </div>
