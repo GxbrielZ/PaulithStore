479f3b8542d4d66dbbebde72dd39d5dd39634e3a
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-07-03 11:27:17 +0200

    Style: change look of this section

--- src/components/Jewellery/JewellerySection.jsx
+++ src/components/Jewellery/JewellerySection.jsx
@@ -14,18 +14,18 @@ const JewellerySection = () => {
     );
 
     return (
-        <div>
-            <div className='flex justify-between items-center mb-4
-            font-merriweather'
-            >
+        <div className="container my-5">
+            <div className="flex justify-between items-center mb-4 font-merriweather text-yellow-900 text-sm">
                 <div>
-                    <label className='mr-2'>Kategoria:</label>
+                    <label className="mr-2">Kategoria:</label>
                     <select
-                        className='p-2 border rounded'
+                        className="p-1 border rounded text-yellow-700 bg-gray-100 cursor-pointer"
                         value={filter}
                         onChange={(e) => setFilter(e.target.value)}
                     >
-                        <option value="all">Wszystko</option>
+                        <option
+                            value="all"
+                            className='cursor-pointer'>Wszystko</option>
                         <option value="neckleases">Naszyjniki</option>
                         <option value="rings">Pierścionki</option>
                         <option value="bracelets">Bransoletki</option>
@@ -34,33 +34,32 @@ const JewellerySection = () => {
                 </div>
                 <div>
                     <button
-                        className='p-2 border rounded'
+                        className="bg-yellow-800 hover:bg-yellow-900 text-white p-1 rounded text-sm"
                         onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')}
                     >
-                        Sortuj: {sortOrder === 'asc' ? 'Low to High' : 'High to Low'}
+                        Sortuj: {sortOrder === 'asc' ? 'cena - malejąco' : 'cena - rosnąco'}
                     </button>
                 </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'
-                >
-                    {sortedProducts.map(product => (
-                        <div
-                            key={product.id}
-                            className='border p-4 rounded shadow'
-                        >
-                            <img
-                                src={product.imageSrc}
-                                alt={product.prodName}
-                                className='w-full h-48 object-cover mb-4'
-                            />
-                            <h2 className='text-lg font-semibold'>
-                                {product.prodName}
-                            </h2>
-                            <p className='text-yellow-800'>
-                                {product.prodPrice}
-                            </p>
-                        </div>
-                    ))}
-                </div>
+            </div>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+                {sortedProducts.map(product => (
+                    <div
+                        key={product.id}
+                        className="border p-4 rounded shadow flex flex-col items-center"
+                    >
+                        <img
+                            src={product.imageSrc}
+                            alt={product.prodName}
+                            className="w-full h-96 object-cover mb-4"
+                        />
+                        <h2 className="text-center text-lg font-semibold mb-2">
+                            {product.prodName}
+                        </h2>
+                        <p className="text-center text-yellow-800 font-bold">
+                            ${product.prodPrice}
+                        </p>
+                    </div>
+                ))}
             </div>
         </div>
     )
